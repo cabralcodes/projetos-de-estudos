0a32b1618a75f4c7f24c3753c03511af33b3d881
Matheus cabral codes <matheusaugustodev.web@gmail.com>
2025-02-13 14:06:21 -0300

    resolvi alguns bugs

--- projeto001/script.js
+++ projeto001/script.js
@@ -7,49 +7,48 @@ const extra3 = document.querySelector("#extra3")
 const text1 = document.querySelector(".txt1");
 const text2 = document.querySelector(".txt2");
 const  text3 = document.querySelector(".txt3");
-const mediaQery = window.matchMedia("(min-width:768px)");
+const texto = document.querySelector(".texto");
+
 
 but1.addEventListener("click", ()=>  {
         but1.setAttribute("style", "display:none;")
         extra.setAttribute("style", "display: block;")
-        text1.setAttribute("style", "margin-bottom:40px;")
+        
 
         setTimeout(() => {
             but1.setAttribute("style", "display:inline-block;")
             extra.setAttribute("style","display: none;")
-            text1.setAttribute("style", "margin-bottom:83px;")
+            
+            
         }, 10000);
     });
 
 but2.addEventListener("click", ()=>{
         but2.setAttribute("style", "display:none;")
         extra2.setAttribute("style", "display:block;")
-        text2.setAttribute("style", "margin-bottom:30px")
 
-       setTimeout(() => {
+        setTimeout(() => {
             but2.setAttribute("style", "display:inline-block;") 
             extra2.setAttribute("style", "display:none;") 
-            text2.setAttribute("style", "margin-bottom:73px")   
+            
         }, 10000);
 
     
-})
+});
 
 but3.addEventListener("click", () =>{
-    
-    
     but3.setAttribute("style", "display:none;")
     extra3.setAttribute("style", "display:block;")
-    text3.setAttribute("style","margin-bottom:50px;")
+
     
 
     setTimeout(() => {
         but3.setAttribute("style", "display:inline-block;")
-        extra3.setAttribute("style","diplay:none;")
-        text3.setAttribute("style","margin-bottom:100px")
+       extra3.setAttribute("style","diplay:none;")
+       
     }, 10000);
-})
 
+})
 
 
 
